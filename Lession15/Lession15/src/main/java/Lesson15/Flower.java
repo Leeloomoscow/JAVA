4b53073b77a651991caf66b6c8489f86ccc636b9
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-08-04 16:56:27 +0300

    First commit

---
+++ Lession15/Lession15/src/main/java/Lesson15/Flower.java
@@ -0,0 +1,12 @@
+package Lesson15;
+
+public class Flower {
+    String name;
+    String color;
+
+    public Flower(){}
+    public Flower (String myName, String myColor){
+        name = myName;
+        color = myColor;
+    }
+}
