0dc372560fa158042ecb4a086d854c5a4b37ab55
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-08-05 17:01:45 +0300

    Урок Поля и методы

---
+++ Lesson17/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        String km = "65";
+        bus.showKm(km);
+
+    }
+}
---
+++ Lesson17/src/main/java/Transmission.java
@@ -0,0 +1,3 @@
+public class Transmission {
+    public String type;
+}
