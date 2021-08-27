e81ab4c23dd2f21ea0a80e3fdb38e8e8e0bbddb9
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-08-27 12:37:14 +0300

    Урок Абстрактные классы и методы

---
+++ Lesson22/src/main/java/AK47.java
@@ -0,0 +1,11 @@
+public class AK47 extends Weapon {
+    @Override
+    public void shoot() {
+
+    }
+
+    @Override
+    public void foo() {
+
+    }
+}
---
+++ Lesson22/src/main/java/Weapon.java
@@ -0,0 +1,12 @@
+public abstract class Weapon {
+    private String name;
+
+    public abstract void shoot();
+    public void show(){
+        System.out.println("");
+    }
+
+    public void foo() {
+    }
+}
+
