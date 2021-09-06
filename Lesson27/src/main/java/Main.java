31b4574855d9808c3935ca0403a873cac3072dd6
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-06 16:48:52 +0300

    Урок "Исключения, ошибки и их иерархия. Блоки try, catch, finaly"

--- Lesson27/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = new String();
-        StringBuilder stringBuilder = new StringBuilder("Java");
-        stringBuilder.deleteCharAt(1);
-        stringBuilder.reverse();
-        System.out.println(stringBuilder);
-    }
-}
